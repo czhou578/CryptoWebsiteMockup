ce88d2e363f1248834b122a40a6c05d3bdc32f97
czhou578 <colizu2020@gmail.com>
2021-09-30 14:15:27 -0700

    all info is displaying on cards

--- marketplace/script.js
+++ marketplace/script.js
@@ -24,11 +24,40 @@ checkboxTemp.addEventListener('change', function() {
 
 
 function showResultsInCard() {
-  console.log(document.getElementsByClassName('resultCard'))
-  // var fetchedResults = fetch('https://api.lunarcrush.com/v2?data=assets&key=gdqfs8abaucjii0k5tfyve&symbol=BTC,LTC&data_points=2').then(res => res.json())
-  // .then((data) => {
-  //   console.log(JSON.stringify(data, null, 2))
-  // })
+  var cards = document.getElementsByClassName('resultCard')
+  var cardName = document.getElementsByClassName('card-name')
+  var cardPrice = document.getElementsByClassName('card-price')
+  var cardVolume= document.getElementsByClassName('volume')
+  var cardMktCap = document.getElementsByClassName('market-cap')
+  var cardMaxSupply = document.getElementsByClassName('max-supply')
+  var cardInterval = document.getElementsByClassName('interval-hour')
+
+  fetch('https://api.lunarcrush.com/v2?data=assets&key=gdqfs8abaucjii0k5tfyve&symbol=BTC,LTC&data_points=2').then(res => res.json())
+  .then((data) => {
+    var coinNames = data.config.symbol.split(',') //get rid of double quotes
+
+    for (let i = 0; i < coinNames.length; i++) {
+      let textNode = document.createTextNode(coinNames[i].slice(1, -1))
+      cardName[i].appendChild(textNode)
+
+      let priceNode = document.createTextNode('Price: $' + data.data[i].price)
+      cardPrice[i].appendChild(priceNode)
+
+      let volumeNode = document.createTextNode('Volume: ' + data.data[i].volume_24h)
+      cardVolume[i].appendChild(volumeNode)
+      
+      let mktCapNode = document.createTextNode('Market Cap: ' + data.data[i].market_cap)
+      cardMktCap[i].appendChild(mktCapNode)
+
+      let supplyNode = document.createTextNode('Supply: ' + data.data[i].max_supply)
+      cardMaxSupply[i].appendChild(supplyNode)
+
+      let intervalNode = document.createTextNode("Interval: Hourly")
+      cardInterval[i].appendChild(intervalNode)
+    }
+
+    console.log(JSON.stringify(data, null, 2))
+  })
 }
 
 showResultsInCard()
@@ -38,6 +67,7 @@ showResultsInCard()
 //style the cards
 //filter using checkbox
 
-//card contents: price, name of coin, volume, market cap, picture, max supply
+//card contents: price, name of coin, volume, market cap, picture, max supply, interval (hour)
+//2x2 grid showing information
 
 // BTC, lTC, ETH, DOGE, BNB, ADA, USDT, AXS, UNI
