0549a6d6eab2559cc986b4a68520ca6d7846045b
czhou578 <colizu2020@gmail.com>
2021-10-01 13:24:37 -0700

    filter working

--- marketplace/script.js
+++ marketplace/script.js
@@ -17,56 +17,74 @@ document.addEventListener('DOMContentLoaded', function() {
 
 var checkboxTemp = document.getElementById('check3')
 var checkboxes = document.querySelectorAll('input[type=checkbox]')
-var rangeArray;
+var rangeArray = []
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('change', function() {
     if (this.checked) {
-      rangeArray = [rangeArray, ...checkboxTemp.value.match(/\d+/g)]
-      console.log(rangeArray)
-      console.log(checkboxTemp.value);
-      console.log(this.checked);
+      rangeArray = rangeArray.concat(checkbox.value.match(/\d+/g))
+      let highestRangeValue = rangeArray[rangeArray.length - 1] //last value
+      console.log(allMarketCaps);
+
+      for (let i = 0; i < allMarketCaps.length; i++) {
+        if (allMarketCaps[i] > highestRangeValue) {
+          let specificCard = document.getElementsByClassName('resultCard')[i]
+          specificCard.remove() //removes card
+          allMarketCaps.splice(i, 1) //deletes price of specific coin from array
+          i--
+          console.log(allMarketCaps);
+        }
+      }
+
+      // for (showedPrice in allMarketCaps) {
+      //   if (showedPrice > highestRangeValue) {
+      //     //remove price
+      //   }
+      // }
     }
   })
 })
 
+var allMarketCaps = []; //array of all the prices of displayed coins
 
-// function showResultsInCard() {
-//   var cards = document.getElementsByClassName('resultCard')
-//   var cardName = document.getElementsByClassName('card-name')
-//   var cardPrice = document.getElementsByClassName('card-price')
-//   var cardVolume= document.getElementsByClassName('volume')
-//   var cardMktCap = document.getElementsByClassName('market-cap')
-//   var cardMaxSupply = document.getElementsByClassName('max-supply')
-//   var cardInterval = document.getElementsByClassName('interval-hour')
+function showResultsInCard() {
+  var cards = document.getElementsByClassName('resultCard')
+  var cardName = document.getElementsByClassName('card-name')
+  var cardPrice = document.getElementsByClassName('card-price')
+  var cardVolume= document.getElementsByClassName('volume')
+  var cardMktCap = document.getElementsByClassName('market-cap')
+  var cardMaxSupply = document.getElementsByClassName('max-supply')
+  var cardInterval = document.getElementsByClassName('interval-hour')
 
-//   fetch('https://api.lunarcrush.com/v2?data=assets&key=gdqfs8abaucjii0k5tfyve&symbol=BTC,LTC&data_points=2').then(res => res.json())
-//   .then((data) => {
-//     var coinNames = data.config.symbol.split(',') //get rid of double quotes
+  fetch('https://api.lunarcrush.com/v2?data=assets&key=gdqfs8abaucjii0k5tfyve&symbol=BTC,LTC&data_points=2').then(res => res.json())
+  .then((data) => {
+    var coinNames = data.config.symbol.split(',') //get rid of double quotes
+    
+    for (let i = 0; i < coinNames.length; i++) {
+      allMarketCaps.push(data.data[i].price)
 
-//     for (let i = 0; i < coinNames.length; i++) {
-//       let textNode = document.createTextNode(coinNames[i].slice(1, -1))
-//       cardName[i].appendChild(textNode)
+      let textNode = document.createTextNode(coinNames[i].slice(1, -1))
+      cardName[i].appendChild(textNode)
 
-//       let priceNode = document.createTextNode('Price: $' + data.data[i].price.toFixed(2))
-//       cardPrice[i].appendChild(priceNode)
+      let priceNode = document.createTextNode('Price: $' + data.data[i].price.toFixed(2))
+      cardPrice[i].appendChild(priceNode)
 
-//       let volumeNode = document.createTextNode('Volume: ' + data.data[i].volume_24h.toLocaleString())
-//       cardVolume[i].appendChild(volumeNode)
+      let volumeNode = document.createTextNode('Volume: ' + data.data[i].volume_24h.toLocaleString())
+      cardVolume[i].appendChild(volumeNode)
       
-//       let mktCapNode = document.createTextNode('Market Cap: ' + data.data[i].market_cap.toLocaleString())
-//       cardMktCap[i].appendChild(mktCapNode)
+      let mktCapNode = document.createTextNode('Market Cap: ' + data.data[i].market_cap.toLocaleString())
+      cardMktCap[i].appendChild(mktCapNode)
 
-//       let supplyNode = document.createTextNode('Supply: ' + parseInt(data.data[i].max_supply).toLocaleString())
-//       cardMaxSupply[i].appendChild(supplyNode)
+      let supplyNode = document.createTextNode('Supply: ' + parseInt(data.data[i].max_supply).toLocaleString())
+      cardMaxSupply[i].appendChild(supplyNode)
 
-//       let intervalNode = document.createTextNode("Interval: Hourly")
-//       cardInterval[i].appendChild(intervalNode)
-//     }
+      let intervalNode = document.createTextNode("Interval: Hourly")
+      cardInterval[i].appendChild(intervalNode)
+    }
 
-//     console.log(JSON.stringify(data, null, 2))
-//   })
-// }
+    console.log(JSON.stringify(data, null, 2))
+  })
+}
 
 //filter by dollar sign and the hyphen
 //split into array, compare the price with the highest range, if its bigger, then filter it out, otherwise, keep it
@@ -74,7 +92,7 @@ checkboxes.forEach((checkbox) => {
 //Edge case: 100+, compare with just 100, and if its over, keep it, else, discard
 
 
-// showResultsInCard()
+showResultsInCard()
 
 //loading the cards on scroll
 //style the cards
