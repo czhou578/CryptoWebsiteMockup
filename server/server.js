5a7c67caee1ac76511f23c09af03bea799915b5e
czhou578 <colizu2020@gmail.com>
2021-11-06 16:57:04 -0700

    created first server using node

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const port = 3000;
+
+const server = http.createServer(function (req, response) {
+  response.write("Colin is here")
+  response.end();
+})
+
+server.listen(port, function(error) {
+  if (error) {
+    console.log('Something went wrong', error);
+  } else {
+    console.log('Server listening on port ' + port);
+  }
+})
